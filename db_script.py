63ab248d8eecc9a310d7d89e371539d30eddd338
dbakti7 <dbakti1605@gmail.com>
2017-04-10 23:48:55 +0800

    Added "area" in db and use google api to retrieve area name (still hardcoded for 100 taxis).

--- db_script.py
+++ db_script.py
@@ -3,6 +3,12 @@ import sqlite3
 db = sqlite3.connect("taxi.db")
 c = db.cursor()
 c.execute('''CREATE TABLE taxi
-             (date text, hour text, minute text, second text, availability int)''')
+             (date text,
+             hour text,
+             minute text,
+             second text,
+             availability int,
+             region text
+             )''')
 db.commit()
 db.close()
--- taxi-api.py
+++ taxi-api.py
@@ -2,6 +2,40 @@ import http.client
 import json
 import sqlite3
 
+def getPostalCode(lng, lat):
+    conn = http.client.HTTPConnection("maps.googleapis.com")
+
+    payload = ""
+
+    conn.request("GET", "/maps/api/geocode/json?latlng=" + str(lat) + "," + str(lng), payload)
+    res = conn.getresponse()
+    data = res.read()
+    data = json.loads(data.decode("utf-8"))
+    if(len(data["results"]) > 0):
+        address = data["results"][0]["address_components"]
+        for addr in address:
+            if addr["types"][0] == "postal_code":
+                return addr["long_name"]
+    return ""
+
+def getArea(lng, lat):
+    conn = http.client.HTTPConnection("maps.googleapis.com")
+
+    payload = ""
+
+    conn.request("GET", "/maps/api/geocode/json?latlng=" + str(lat) + "," + str(lng), payload)
+    res = conn.getresponse()
+    data = res.read()
+    data = json.loads(data.decode("utf-8"))
+    if(len(data["results"]) > 0):
+        address = data["results"][0]["address_components"]
+        for addr in address:
+            if addr["types"][0] == "neighborhood":
+                return addr["long_name"]
+    return ""
+
+
+
 db = sqlite3.connect("taxi.db")
 c = db.cursor()
 
@@ -11,6 +45,7 @@ payload = ""
 
 headers = { 'api-key': "eSP8hew3DJNjC0sErarllnOiq3ol2QKY" }
 
+date = "2017-03-15"
 for x in range(0, 25):
     hour = "0" + str(x) if x < 10 else str(x) #must be in two-digit format
     minute = "00"
@@ -23,9 +58,23 @@ for x in range(0, 25):
 
     data = json.loads(data.decode("utf-8"))
     print("Time: " + date + " " + hour + ":" + minute + ":" + second)
-    availability = len(data["features"][0]["geometry"]["coordinates"])
+    coords = data["features"][0]["geometry"]["coordinates"]
+    availability = len(coords)
     print("Number of available taxis: " + str(availability))
-
-    c.execute("INSERT INTO taxi VALUES(?,?,?,?,?)", (date, hour, minute, second, availability))
+    if(availability > 0):
+        count = 0
+        areas = dict()
+        for coord in coords:
+            count += 1
+            if(count == 100):
+                break
+            area = getArea(data["features"][0]["geometry"]["coordinates"][0][0], data["features"][0]["geometry"]["coordinates"][0][1])
+            if area != "":
+                if(area in areas):
+                    areas[area] += 1
+                else:
+                    areas[area] = 1
+        for area in areas:
+            c.execute("INSERT INTO taxi VALUES(?,?,?,?,?,?)", (date, hour, minute, second, areas[area], area))
 db.commit()
 db.close()
